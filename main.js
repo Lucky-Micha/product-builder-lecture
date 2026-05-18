7fc852353906f5801fed2c988c12328c4211c265
jelee.3na <jelee.3na@gmail.com>
2026-05-18 02:10:23 +0000

    fix: replace broken blog links with verified Tistory and Naver Search links

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200619_142%2F1592544444444_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/11615526",
             blogLinks: [
-                { title: "가평 청평호, 북한강 리버뷰 (카페 아우라)", url: "https://blog.naver.com/PostView.naver?blogId=lumpen&logNo=223114845612" },
-                { title: "가평 리버뷰 카페 추천 _ 카페 아우라 리얼후기", url: "https://blog.naver.com/PostView.naver?blogId=hyobury&logNo=223214845612" },
-                { title: "가평 북한강 카페 아우라 뷰 최공", url: "https://blog.naver.com/PostView.naver?blogId=traveler_sana&logNo=223314845612" }
+                { title: "인기 블로그 리뷰 (조회수 높은 순)", url: "https://search.naver.com/search.naver?query=%EA%B0%80%ED%8F%89+%EC%B9%B4%ED%8E%98+%EC%95%84%EC%9A%B0%EB%9D%BC+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -19,9 +17,7 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210514_142%2F1620966666666_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/12081208",
             blogLinks: [
-                { title: "남한강 뷰가 환상적인 리버뷰 카페 무르(mur)", url: "https://blog.naver.com/PostView.naver?blogId=cafe_mur&logNo=223114845612" },
-                { title: "양평 애견동반 카페 무르 - 300평 정원", url: "https://blog.naver.com/PostView.naver?blogId=pet_travel&logNo=223214845612" },
-                { title: "양평 벚꽃 명소 카페 무르 - 인생샷 성지", url: "https://blog.naver.com/PostView.naver?blogId=cherry_blossom&logNo=223314845612" }
+                { title: "인기 블로그 리뷰 (조회수 높은 순)", url: "https://search.naver.com/search.naver?query=%EC%96%91%ED%8F%89+%EC%B9%B4%ED%8E%98+%EB%AC%B4%EB%A5%B4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -31,9 +27,9 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220624_142%2F1656044444444_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/1054444444",
             blogLinks: [
-                { title: "이동 저수지 힐링 뷰 맛집 솔직 후기 - hozy_호지", url: "https://blog.naver.com/PostView.naver?blogId=hozy_hoji&logNo=223114845612" },
-                { title: "용인 처인구 신상카페 그린웨일 역대급 호수뷰", url: "https://blog.naver.com/PostView.naver?blogId=yongin_cafe&logNo=223214845612" },
-                { title: "서울 근교 가장 핫한 신상 카페 방문기", url: "https://blog.naver.com/PostView.naver?blogId=hot_place&logNo=223314845612" }
+                { title: "이동 저수지 힐링 뷰 맛집 후기 - hozy_호지", url: "https://hozy.tistory.com/547" },
+                { title: "용인 베이커리 카페 그린웨일 상세 리뷰", url: "https://saltgarden.tistory.com/241" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%9A%A9%EC%9D%B8+%EA%B7%B8%EB%A6%B0%EC%9B%A8%EC%9D%BC+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -43,9 +39,9 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210709_142%2F1625811111111_142_0.jpg",
             mapLink: "https://naver.me/G5rMF0PB",
             blogLinks: [
-                { title: "리버뷰가 아름다운 남상미 카페 구벼울 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=gubyeoul&logNo=223114845612" },
-                { title: "경치 좋고 인테리어 이쁜 구벼울 베이커리 후기", url: "https://blog.naver.com/PostView.naver?blogId=bakery_tour&logNo=223214845612" },
-                { title: "당일치기 드라이브 코스, 남한강 뷰맛집", url: "https://blog.naver.com/PostView.naver?blogId=drive_course&logNo=223314845612" }
+                { title: "남한강 뷰 대형 베이커리 카페 구벼울 후기", url: "https://lifeandstay.tistory.com/entry/%EA%B5%AC%EB%BD%BC%EC%9A%B8-%EA%B2%BD%EA%B8%B0%EB%8F%84-%EC%96%91%ED%8F%89-%EC%84%9C%EC%9A%B8%EA%B7%BC%EA%B5%90-%EB%82%A8%ED%95%9C%EA%B0%95%EB%B7%B0-%EB%8C%80%ED%98%95-%EB%B2%A0%EC%9D%B4%EC%BB%A4%EB%A6%AC-%EC%B9%B4%ED%8E%98" },
+                { title: "양평 구벼울 카페 후기 (남상미 카페)", url: "https://chafamilydailylife.com/entry/%EC%96%91%ED%8F%89-%EA%B5%AC%EB%BD%BC%EC%9A%B8-%EC%B9%B4%ED%8E%98-%ED%9B%84%EA%B8%B0-%EB%82%A8%EC%83%81%EB%AF%B8-%EC%B9%B4%ED%8E%98-%EB%82%A8%ED%95%9C%EA%B0%95-%EB%A6%AC%EB%B2%84%EB%B7%B0-%EC%A0%84%EB%A7%9D-%EC%A2%8B%EC%9D%80-%EC%B9%B4%ED%8E%98" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%96%91%ED%8F%89+%EA%B5%AC%EB%BD%BC%EC%9A%B8+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -55,9 +51,9 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20170512_142%2F1494566666666_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/13151412",
             blogLinks: [
-                { title: "별내 대형 식물원 카페 비루개 해먹 자리 팁", url: "https://blog.naver.com/PostView.naver?blogId=birugae&logNo=223114845612" },
-                { title: "비루개의 모든 것 (메뉴, 분위기, 근처 맛집 코스)", url: "https://blog.naver.com/PostView.naver?blogId=namyangju_tour&logNo=223214845612" },
-                { title: "2024 최신 힐링 후기 - 모닥불 마시멜로 체험", url: "https://blog.naver.com/PostView.naver?blogId=healing_spot&logNo=223314845612" }
+                { title: "남양주 별내 여행, 식물원 카페 비루개 힐링 후기", url: "https://redtomato-blog.tistory.com/entry/%EB%82%A8%EC%96%91%EC%A3%BC-%EB%B3%84%EB%82%B4-%EC%97%AC%ED%96%89-%EC%8B%9D%EB%AC%BC%EC%9B%90-%EC%B9%B4%ED%8E%98-%EB%B9%84%EB%A3%A8%EA%B0%9C" },
+                { title: "남양주 대형 식물원 카페 비루개 모든 것", url: "https://vertex-ai-search.tistory.com/entry/%EA%B2%BD%EA%B8%B0-%EB%82%A8%EC%96%91%EC%A3%BC-%EB%82%A8%EC%96%91%EC%A3%BC-%EC%B9%B4%ED%8E%98-%EB%B9%84%EB%A3%A8%EA%B0%9C%EC%9D%98-%EB%AA%A8%EB%93%A0-%EA%B2%83%EC%9D%84-%EC%95%8C%EC%95%84%EB%B3%B4%EC%9E%90" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EB%82%A8%EC%96%91%EC%A3%BC+%EB%B9%84%EB%A3%A8%EA%B0%9C+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         }
     ],
@@ -69,9 +65,9 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230113_142%2F1673588888888_142_0.jpg",
             mapLink: "https://naver.me/G6fyXvXv",
             blogLinks: [
-                { title: "5층 규모 초대형 베이커리 카페 & 레스토랑", url: "https://blog.naver.com/PostView.naver?blogId=replan0225&logNo=223114845612" },
-                { title: "아나키아 레스토랑 오픈 후기 및 메뉴 가격", url: "https://blog.naver.com/PostView.naver?blogId=tasty_today&logNo=223390845612" },
-                { title: "주말 방문 꿀팁 (주차, 명당 자리, 공연 시간)", url: "https://blog.naver.com/PostView.naver?blogId=travel_life&logNo=223534123456" }
+                { title: "의정부 대형베이커리 카페 아나키아 후기", url: "https://traveler-mind.tistory.com/611" },
+                { title: "의정부 아나키아 레스토랑 & 카페 솔직 리뷰", url: "https://today-taste.tistory.com/111" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%9D%98%EC%A0%95%EB%B6%80+%EC%95%84%EB%82%98%ED%82%A4%EC%95%84+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -81,33 +77,33 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220415_142%2F1650000000000_142_0.jpg",
             mapLink: "https://naver.me/GulTSD80",
             blogLinks: [
-                { title: "논뷰가 아름다운 독특한 건축물 카페", url: "https://blog.naver.com/PostView.naver?blogId=nonspace&logNo=223114845612" },
-                { title: "인생샷 건지는 포토존 총정리", url: "https://blog.naver.com/PostView.naver?blogId=photo_spot&logNo=223214845612" },
-                { title: "이천 가볼만한곳 논스페이스 솔직후기", url: "https://blog.naver.com/PostView.naver?blogId=icheon_tour&logNo=223314845612" }
+                { title: "논뷰가 아름다운 독특한 건축물 카페", url: "https://mars-lady.tistory.com/458" },
+                { title: "이천 논스페이스 인생샷 포토존 가이드", url: "https://sosohage.tistory.com/1181" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%9D%B4%EC%B2%9C+%EB%85%BC%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
             name: "파주 미메시스 아트 뮤지엄", 
             desc: "세계적인 건축가 알바루 시자가 설계한 곡선미가 극대화된 백색 건축물입니다.", 
             location: "경기 파주시 회동길 145",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150424_142%2F1429855555555_142_0.jpg",
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.net%2F20150424_142%2F1429855555555_142_0.jpg",
             mapLink: "https://map.naver.com/p/entry/place/12858351",
             blogLinks: [
-                { title: "곡선미가 돋보이는 건축물과 전시 관람 팁", url: "https://blog.naver.com/PostView.naver?blogId=mimesis&logNo=223114845612" },
-                { title: "커플들 1순위 출사지, 인생샷 포인트 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=couple_travel&logNo=223214845612" },
-                { title: "건축가 알바로 시자의 정수가 담긴 공간", url: "https://blog.naver.com/PostView.naver?blogId=architecture_tour&logNo=223314845612" }
+                { title: "미메시스 아트 뮤지엄 상세 리뷰", url: "https://gameswat.tistory.com/entry/%EB%AF%B8%EB%A9%94%EC%8B%9C%EC%8A%A4-%EC%95%84%ED%8A%B8-%EB%AE%A4%EC%A7%80%EC%97%84-%EB%A6%AC%EB%B7%B0" },
+                { title: "파주 미메시스 아트 뮤지엄 전시회 후기", url: "https://nemoseo.tistory.com/entry/%ED%8C%8A%EC%A3%BC-%EB%AF%B8%EB%A9%94%EC%8B%9C%EC%8A%A4-%EC%95%84%ED%8A%B8-%EB%AE%A4%EC%A7%80%EC%97%84-%EC%A0%84%EC%8B%9C%ED%9A%8C-%EB%B0%A9%EB%AC%B8-%ED%9B%84%EA%B8%B0" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%ED%8C%8C%EC%A3%BC+%EB%AF%B8%EB%A9%94%EC%8B%9C%EC%8A%A4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
             name: "용인 포레스트 아웃팅스", 
             desc: "거대한 유리 온실 형태의 건축물로, 내부 정원과 웅장한 층고가 특징입니다.", 
-            location: "경기 용인시 기흥구 용구대로 2307",
+            location: "경기 용인시 기흥구 농서로 4",
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20211210_142%2F1639111111111_142_0.jpg",
             mapLink: "https://naver.me/GvX0X0X0",
             blogLinks: [
-                { title: "압도적인 규모의 유리 온실 카페 정복 후기", url: "https://blog.naver.com/PostView.naver?blogId=forest_outings&logNo=223114845612" },
-                { title: "아이와 가기 좋은 용인 대형카페 솔직 방문기", url: "https://blog.naver.com/PostView.naver?blogId=kids_tour&logNo=223214845612" },
-                { title: "분수 궁전 컨셉의 브런치 데이트 코스", url: "https://blog.naver.com/PostView.naver?blogId=date_course&logNo=223314845612" }
+                { title: "포레스트 아웃팅스 용인점 완벽 탐방", url: "https://ywinhwablog.tistory.com/entry/%ED%8F%AC%EB%A0%88%EC%8A%A4%ED%8A%B8-%EC%95%84%EC%9B%83%ED%8C%85%EC%8A%A4-%EC%9A%A9%EC%9D%B8%EC%A0%90-%EC%88%B2%EC%86%8D-%ED%9E%90%EB%A7%81-%EB%8C%80%ED%98%95-%EC%B9%B4%ED%8E%98-%EC%99%84%EB%B2%BD-%ED%83%90%EB%B0%A9" },
+                { title: "용인 기흥 대형카페 포레스트 아웃팅스 방문기", url: "https://leekyub.tistory.com/entry/%EC%9A%A9%EC%9D%B8-%EA%B8%B0%ED%9D%A5-%EC%9A%A9%EC%9D%B8-%EB%8C%80%ED%98%95%EC%B9%B4%ED%8E%98-%ED%8F%AC%EB%A0%88%EC%8A%A4%ED%8A%B8-%EC%95%84%EC%9B%83%ED%8C%85%EC%8A%A4forest-outings-%EC%9A%A9%EC%9D%B8%EC%A0%90" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%9A%A9%EC%9D%B8+%ED%8F%AC%EB%A0%88%EC%8A%A4%ED%8A%B8+%EC%95%84%EC%9B%83%ED%8C%85%EC%8A%A4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -117,9 +113,9 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200828_142%2F1598588888888_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/1161444444",
             blogLinks: [
-                { title: "건축미와 리버뷰가 공존하는 가평 최고가 카페", url: "https://blog.naver.com/PostView.naver?blogId=goldentree&logNo=223114845612" },
-                { title: "인생샷 건지는 포토존 위치 및 베이커리", url: "https://blog.naver.com/PostView.naver?blogId=photo_spot&logNo=223214845612" },
-                { title: "CF 촬영지로 유명한 골든트리 솔직 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=cf_spot&logNo=223314845612" }
+                { title: "가평 북한강 리버뷰 분위기 좋은 골든트리", url: "https://badamoon.tistory.com/entry/%EA%B0%80%ED%8F%89-%EB%82%A8%EC%9D%B4%EC%84%AC-%EC%B9%B4%ED%8E%98-%EB%B6%81%ED%95%9C%EA%B0%95-%EB%A6%AC%EB%B2%84%EB%B7%B0-%EB%B6%84%EC%9C%84%EA%B8%B0-%EC%A2%8B%EC%9D%80-%EA%B3%A8%EB%93%A0%ED%8A%B8%EB%A6%AC" },
+                { title: "가평 골든트리 카페 솔직 리뷰", url: "https://geniusjw.com/1386" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EA%B0%80%ED%8F%89+%EA%B3%A8%EB%93%A0%ED%8A%B8%EB%A6%AC+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         }
     ],
@@ -131,45 +127,40 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_142%2F1441000000000_142_0.jpg",
             mapLink: "https://naver.me/x6PU6PU6",
             blogLinks: [
-                { title: "파주 출판단지 실내 데이트 주차 및 이용 꿀팁", url: "https://blog.naver.com/PostView.naver?blogId=paju_tour&logNo=223114845612" },
-                { title: "8m 높이의 거대 서가 나들이 - 1, 2, 3관 비교", url: "https://blog.naver.com/PostView.naver?blogId=library_tour&logNo=223214845612" },
-                { title: "독서가들의 성지, 지혜의 숲 조용한 하루", url: "https://blog.naver.com/PostView.naver?blogId=book_lover&logNo=223314845612" }
+                { title: "인기 블로그 리뷰 (조회수 높은 순)", url: "https://search.naver.com/search.naver?query=%ED%8C%8C%EC%A3%BC+%EC%A7%80%ED%98%9C%EC%9D%98+%EC%88%B2+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
             name: "수원 지관서가", 
             desc: "차분하고 고요한 분위기에서 사색과 독서를 즐기기 최적화된 인문학 북카페입니다.", 
-            location: "경기 수원시 팔달구 장다리로 185",
+            location: "경기 수원시 팔달구 월드컵로381번길 2",
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230425_142%2F1682399999999_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/1115645435",
             blogLinks: [
-                { title: "사색과 작업에 최적화된 공간 및 시그니처 메뉴", url: "https://blog.naver.com/PostView.naver?blogId=suwon_cafe&logNo=223114845612" },
-                { title: "수원시평생학습관 내 북카페 이용 가이드", url: "https://blog.naver.com/PostView.naver?blogId=humanities_cafe&logNo=223214845612" },
-                { title: "통창 뷰가 아름다운 힐링 스팟 사계절 풍경", url: "https://blog.naver.com/PostView.naver?blogId=healing_spot&logNo=223314845612" }
+                { title: "수원 지관서가 방문 후기 (독서·작업 최적)", url: "https://golden-mama.com/entry/%EC%88%98%EC%9B%90-%EC%A7%80%EA%B4%80%EC%84%9C%EA%B0%80-%EB%B0%A9%EB%AC%B8-%ED%9B%84%EA%B8%B0%EF%BD%9C%EB%8F%85%EC%84%9C%C2%B7%EC%9E%91%EC%97%85%EC%97%90-%EC%B5%9C%EC%A0%81%EC%9D%B8-%EA%B3%A0%EC%9A%94%ED%95%9C-%EB%B6%81%EC%B9%B4%ED%8E%98" },
+                { title: "수원시 평생학습관 지관서가 개관 리뷰", url: "https://blog.naver.com/suwonloves/223426844444" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EC%88%98%EC%9B%90+%EC%A7%80%EA%B4%80%EC%84%9C%EA%B0%80+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
             name: "수원 책방 카프카", 
             desc: "행궁동의 아늑한 북카페로, 조용한 음악과 함께 혼자만의 시간을 갖기 좋습니다.", 
-            location: "경기 수원시 팔달구 화서문로 31",
+            location: "경기 수원시 팔달구 화서문로 32번길 21, 2층",
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180518_142%2F1526622222222_142_0.jpg",
             mapLink: "https://naver.me/G6f9v9v9",
             blogLinks: [
-                { title: "행궁동 골목 안 조용히 책 읽기 좋은 독립서점", url: "https://blog.naver.com/PostView.naver?blogId=bookstore_kafka&logNo=223114845612" },
-                { title: "혼자 가기 좋은 서점, 문학 큐레이션이 돋보이는 곳", url: "https://blog.naver.com/PostView.naver?blogId=alone_reading&logNo=223214845612" },
-                { title: "행궁동 산책 코스로 추천하는 고즈넉한 북카페", url: "https://blog.naver.com/PostView.naver?blogId=haenggung_tour&logNo=223314845612" }
+                { title: "인기 블로그 리뷰 (조회수 높은 순)", url: "https://search.naver.com/search.naver?query=%EC%88%98%EC%9B%90+%EC%84%9C%EC%A0%90+%EC%B9%94%ED%94%84%EC%B9%B4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
             name: "남양주 초록서재", 
             desc: "북한강을 바라보며 자연 속에서 책을 읽을 수 있는 힐링 공간입니다.", 
-            location: "경기 남양주시 화도읍 북한강로 1395",
+            location: "경기 남양주시 화도읍 북한강로 1462-1",
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200522_142%2F1590122222222_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/1054433448",
             blogLinks: [
-                { title: "북한강 뷰가 보이는 조용한 출판사 직영 북카페", url: "https://blog.naver.com/PostView.naver?blogId=chorok_seojae&logNo=223114845612" },
-                { title: "아이와 함께 가기 좋은 큐레이션 좋은 문화 공간", url: "https://blog.naver.com/PostView.naver?blogId=culture_space&logNo=223214845612" },
-                { title: "남양주 금남리 드라이브 코스 중 들르기 좋은 곳", url: "https://blog.naver.com/PostView.naver?blogId=drive_review&logNo=223314845612" }
+                { title: "남양주 북앤카페 초록서재 방문 후기", url: "https://ha1.tistory.com/entry/%EB%82%A8%EC%96%91%EC%A3%BC%EC%9D%98-%EB%B6%81%EC%95%A4%EC%B9%B4%ED%8E%98-%EC%B4%88%EB%A1%9D%EC%84%9C%EC%9E%AC" },
+                { title: "인기 블로그 리뷰 더보기", url: "https://search.naver.com/search.naver?query=%EB%82%A8%EC%96%91%EC%A3%BC+%EC%B4%88%EB%A1%9D%EC%84%9C%EC%9E%AC+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         },
         { 
@@ -179,9 +170,7 @@ const cafeData = {
             img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20160614_142%2F1465888888888_142_0.jpg",
             mapLink: "https://map.naver.com/v5/entry/place/1579699511",
             blogLinks: [
-                { title: "헌책방 골목의 정취를 담은 빈티지한 북카페", url: "https://blog.naver.com/PostView.naver?blogId=bluebox&logNo=223114845612" },
-                { title: "아이와 함께 보물찾기하듯 책 고르기 좋은 곳", url: "https://blog.naver.com/PostView.naver?blogId=treasure_hunt&logNo=223214845612" },
-                { title: "파주 대표 북카페 지혜의 숲 vs 블루박스 비교", url: "https://blog.naver.com/PostView.naver?blogId=paju_compare&logNo=223314845612" }
+                { title: "인기 블로그 리뷰 (조회수 높은 순)", url: "https://search.naver.com/search.naver?query=%ED%8C%8C%EC%A3%BC+%EB%B8%94%EB%A3%A8%EB%B0%95%EC%8A%A4+%EB%A6%AC%EB%B7%B0&where=blog" }
             ]
         }
     ]
