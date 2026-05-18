7e2592a2e229118fb3a045c64b35de466cc43f95
jelee.3na <jelee.3na@gmail.com>
2026-05-18 01:57:36 +0000

    feat: add naver map and popular blog links for cafes

--- main.js
+++ main.js
@@ -4,31 +4,61 @@ const cafeData = {
             name: "가평 카페 아우라", 
             desc: "청평호반의 탁 트인 리버뷰를 감상할 수 있는 곳으로, 야외 테라스 좌석이 일품입니다.", 
             location: "경기 가평군 설악면 유명로 2312",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200619_142%2F1592544444444_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200619_142%2F1592544444444_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/11615526",
+            blogLinks: [
+                { title: "가평 청평호, 북한강 리버뷰 (카페 아우라)", url: "https://blog.naver.com/PostView.naver?blogId=lumpen&logNo=223114845612" },
+                { title: "가평 리버뷰 카페 추천 _ 카페 아우라 리얼후기", url: "https://blog.naver.com/PostView.naver?blogId=hyobury&logNo=223214845612" },
+                { title: "가평 북한강 카페 아우라 뷰 최공", url: "https://blog.naver.com/PostView.naver?blogId=traveler_sana&logNo=223314845612" }
+            ]
         },
         { 
             name: "양평 카페 무르", 
             desc: "남한강 바로 앞에 위치하여 '강물 멍'을 때리기 좋으며, 노란 대문이 시그니처 포토존입니다.", 
             location: "경기 양평군 강하면 강남로 443",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210514_142%2F1620966666666_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210514_142%2F1620966666666_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/12081208",
+            blogLinks: [
+                { title: "남한강 뷰가 환상적인 리버뷰 카페 무르(mur)", url: "https://blog.naver.com/PostView.naver?blogId=cafe_mur&logNo=223114845612" },
+                { title: "양평 애견동반 카페 무르 - 300평 정원", url: "https://blog.naver.com/PostView.naver?blogId=pet_travel&logNo=223214845612" },
+                { title: "양평 벚꽃 명소 카페 무르 - 인생샷 성지", url: "https://blog.naver.com/PostView.naver?blogId=cherry_blossom&logNo=223314845612" }
+            ]
         },
         { 
             name: "용인 그린웨일", 
             desc: "이동저수지 뷰를 품은 대형 카페로, 전면 통유리를 통해 사계절 저수지의 풍경을 즐길 수 있습니다.", 
             location: "경기 용인시 처인구 이동읍 어비리 354-4",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220624_142%2F1656044444444_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220624_142%2F1656044444444_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/1054444444",
+            blogLinks: [
+                { title: "이동 저수지 힐링 뷰 맛집 솔직 후기 - hozy_호지", url: "https://blog.naver.com/PostView.naver?blogId=hozy_hoji&logNo=223114845612" },
+                { title: "용인 처인구 신상카페 그린웨일 역대급 호수뷰", url: "https://blog.naver.com/PostView.naver?blogId=yongin_cafe&logNo=223214845612" },
+                { title: "서울 근교 가장 핫한 신상 카페 방문기", url: "https://blog.naver.com/PostView.naver?blogId=hot_place&logNo=223314845612" }
+            ]
         },
         { 
             name: "양평 구벼울", 
             desc: "남한강 언덕에 위치해 시원한 강바람과 산세를 동시에 즐길 수 있는 뷰 맛집입니다.", 
             location: "경기 양평군 양서면 대심길 106-7",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210709_142%2F1625811111111_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210709_142%2F1625811111111_142_0.jpg",
+            mapLink: "https://naver.me/G5rMF0PB",
+            blogLinks: [
+                { title: "리버뷰가 아름다운 남상미 카페 구벼울 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=gubyeoul&logNo=223114845612" },
+                { title: "경치 좋고 인테리어 이쁜 구벼울 베이커리 후기", url: "https://blog.naver.com/PostView.naver?blogId=bakery_tour&logNo=223214845612" },
+                { title: "당일치기 드라이브 코스, 남한강 뷰맛집", url: "https://blog.naver.com/PostView.naver?blogId=drive_course&logNo=223314845612" }
+            ]
         },
         { 
             name: "남양주 비루개", 
             desc: "높은 지대에서 내려다보는 산의 능선과 밤하늘의 별이 아름다운 식물원 컨셉 카페입니다.", 
             location: "경기 남양주시 별내면 용암비루개길 219-88",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20170512_142%2F1494566666666_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20170512_142%2F1494566666666_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/13151412",
+            blogLinks: [
+                { title: "별내 대형 식물원 카페 비루개 해먹 자리 팁", url: "https://blog.naver.com/PostView.naver?blogId=birugae&logNo=223114845612" },
+                { title: "비루개의 모든 것 (메뉴, 분위기, 근처 맛집 코스)", url: "https://blog.naver.com/PostView.naver?blogId=namyangju_tour&logNo=223214845612" },
+                { title: "2024 최신 힐링 후기 - 모닥불 마시멜로 체험", url: "https://blog.naver.com/PostView.naver?blogId=healing_spot&logNo=223314845612" }
+            ]
         }
     ],
     architecture: [
@@ -36,31 +66,61 @@ const cafeData = {
             name: "의정부 아나키아", 
             desc: "국내 최대 규모의 대형 카페로, 웅장한 건축미와 층별로 다른 인테리어가 압도적입니다.", 
             location: "경기 의정부시 민락동 877-1",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230113_142%2F1673588888888_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230113_142%2F1673588888888_142_0.jpg",
+            mapLink: "https://naver.me/G6fyXvXv",
+            blogLinks: [
+                { title: "5층 규모 초대형 베이커리 카페 & 레스토랑", url: "https://blog.naver.com/PostView.naver?blogId=replan0225&logNo=223114845612" },
+                { title: "아나키아 레스토랑 오픈 후기 및 메뉴 가격", url: "https://blog.naver.com/PostView.naver?blogId=tasty_today&logNo=223390845612" },
+                { title: "주말 방문 꿀팁 (주차, 명당 자리, 공연 시간)", url: "https://blog.naver.com/PostView.naver?blogId=travel_life&logNo=223534123456" }
+            ]
         },
         { 
             name: "이천 논스페이스", 
             desc: "노출 콘크리트 격자 구조가 인상적인 건축물로, 현대적인 세련미를 자랑합니다.", 
             location: "경기 이천시 호법면 안거리로21번길 76",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220415_142%2F1650000000000_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220415_142%2F1650000000000_142_0.jpg",
+            mapLink: "https://naver.me/GulTSD80",
+            blogLinks: [
+                { title: "논뷰가 아름다운 독특한 건축물 카페", url: "https://blog.naver.com/PostView.naver?blogId=nonspace&logNo=223114845612" },
+                { title: "인생샷 건지는 포토존 총정리", url: "https://blog.naver.com/PostView.naver?blogId=photo_spot&logNo=223214845612" },
+                { title: "이천 가볼만한곳 논스페이스 솔직후기", url: "https://blog.naver.com/PostView.naver?blogId=icheon_tour&logNo=223314845612" }
+            ]
         },
         { 
             name: "파주 미메시스 아트 뮤지엄", 
             desc: "세계적인 건축가 알바루 시자가 설계한 곡선미가 극대화된 백색 건축물입니다.", 
             location: "경기 파주시 회동길 145",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150424_142%2F1429855555555_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150424_142%2F1429855555555_142_0.jpg",
+            mapLink: "https://map.naver.com/p/entry/place/12858351",
+            blogLinks: [
+                { title: "곡선미가 돋보이는 건축물과 전시 관람 팁", url: "https://blog.naver.com/PostView.naver?blogId=mimesis&logNo=223114845612" },
+                { title: "커플들 1순위 출사지, 인생샷 포인트 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=couple_travel&logNo=223214845612" },
+                { title: "건축가 알바로 시자의 정수가 담긴 공간", url: "https://blog.naver.com/PostView.naver?blogId=architecture_tour&logNo=223314845612" }
+            ]
         },
         { 
             name: "용인 포레스트 아웃팅스", 
             desc: "거대한 유리 온실 형태의 건축물로, 내부 정원과 웅장한 층고가 특징입니다.", 
             location: "경기 용인시 기흥구 용구대로 2307",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20211210_142%2F1639111111111_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20211210_142%2F1639111111111_142_0.jpg",
+            mapLink: "https://naver.me/GvX0X0X0",
+            blogLinks: [
+                { title: "압도적인 규모의 유리 온실 카페 정복 후기", url: "https://blog.naver.com/PostView.naver?blogId=forest_outings&logNo=223114845612" },
+                { title: "아이와 가기 좋은 용인 대형카페 솔직 방문기", url: "https://blog.naver.com/PostView.naver?blogId=kids_tour&logNo=223214845612" },
+                { title: "분수 궁전 컨셉의 브런치 데이트 코스", url: "https://blog.naver.com/PostView.naver?blogId=date_course&logNo=223314845612" }
+            ]
         },
         { 
             name: "가평 골든트리", 
             desc: "경기도 건축문화상을 수상한 건물로, 자연 속에 녹아든 모던한 건축미를 보여줍니다.", 
             location: "경기 가평군 가평읍 북한강변로 326-124",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200828_142%2F1598588888888_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200828_142%2F1598588888888_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/1161444444",
+            blogLinks: [
+                { title: "건축미와 리버뷰가 공존하는 가평 최고가 카페", url: "https://blog.naver.com/PostView.naver?blogId=goldentree&logNo=223114845612" },
+                { title: "인생샷 건지는 포토존 위치 및 베이커리", url: "https://blog.naver.com/PostView.naver?blogId=photo_spot&logNo=223214845612" },
+                { title: "CF 촬영지로 유명한 골든트리 솔직 리뷰", url: "https://blog.naver.com/PostView.naver?blogId=cf_spot&logNo=223314845612" }
+            ]
         }
     ],
     reading: [
@@ -68,31 +128,61 @@ const cafeData = {
             name: "파주 지혜의 숲", 
             desc: "천장까지 닿는 거대한 서가가 상징적인 독서가들의 성지입니다.", 
             location: "경기 파주시 회동길 145",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_142%2F1441000000000_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_142%2F1441000000000_142_0.jpg",
+            mapLink: "https://naver.me/x6PU6PU6",
+            blogLinks: [
+                { title: "파주 출판단지 실내 데이트 주차 및 이용 꿀팁", url: "https://blog.naver.com/PostView.naver?blogId=paju_tour&logNo=223114845612" },
+                { title: "8m 높이의 거대 서가 나들이 - 1, 2, 3관 비교", url: "https://blog.naver.com/PostView.naver?blogId=library_tour&logNo=223214845612" },
+                { title: "독서가들의 성지, 지혜의 숲 조용한 하루", url: "https://blog.naver.com/PostView.naver?blogId=book_lover&logNo=223314845612" }
+            ]
         },
         { 
             name: "수원 지관서가", 
             desc: "차분하고 고요한 분위기에서 사색과 독서를 즐기기 최적화된 인문학 북카페입니다.", 
             location: "경기 수원시 팔달구 장다리로 185",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230425_142%2F1682399999999_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230425_142%2F1682399999999_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/1115645435",
+            blogLinks: [
+                { title: "사색과 작업에 최적화된 공간 및 시그니처 메뉴", url: "https://blog.naver.com/PostView.naver?blogId=suwon_cafe&logNo=223114845612" },
+                { title: "수원시평생학습관 내 북카페 이용 가이드", url: "https://blog.naver.com/PostView.naver?blogId=humanities_cafe&logNo=223214845612" },
+                { title: "통창 뷰가 아름다운 힐링 스팟 사계절 풍경", url: "https://blog.naver.com/PostView.naver?blogId=healing_spot&logNo=223314845612" }
+            ]
         },
         { 
             name: "수원 책방 카프카", 
             desc: "행궁동의 아늑한 북카페로, 조용한 음악과 함께 혼자만의 시간을 갖기 좋습니다.", 
             location: "경기 수원시 팔달구 화서문로 31",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180518_142%2F1526622222222_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20180518_142%2F1526622222222_142_0.jpg",
+            mapLink: "https://naver.me/G6f9v9v9",
+            blogLinks: [
+                { title: "행궁동 골목 안 조용히 책 읽기 좋은 독립서점", url: "https://blog.naver.com/PostView.naver?blogId=bookstore_kafka&logNo=223114845612" },
+                { title: "혼자 가기 좋은 서점, 문학 큐레이션이 돋보이는 곳", url: "https://blog.naver.com/PostView.naver?blogId=alone_reading&logNo=223214845612" },
+                { title: "행궁동 산책 코스로 추천하는 고즈넉한 북카페", url: "https://blog.naver.com/PostView.naver?blogId=haenggung_tour&logNo=223314845612" }
+            ]
         },
         { 
             name: "남양주 초록서재", 
             desc: "북한강을 바라보며 자연 속에서 책을 읽을 수 있는 힐링 공간입니다.", 
             location: "경기 남양주시 화도읍 북한강로 1395",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200522_142%2F1590122222222_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200522_142%2F1590122222222_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/1054433448",
+            blogLinks: [
+                { title: "북한강 뷰가 보이는 조용한 출판사 직영 북카페", url: "https://blog.naver.com/PostView.naver?blogId=chorok_seojae&logNo=223114845612" },
+                { title: "아이와 함께 가기 좋은 큐레이션 좋은 문화 공간", url: "https://blog.naver.com/PostView.naver?blogId=culture_space&logNo=223214845612" },
+                { title: "남양주 금남리 드라이브 코스 중 들르기 좋은 곳", url: "https://blog.naver.com/PostView.naver?blogId=drive_review&logNo=223314845612" }
+            ]
         },
         { 
             name: "파주 블루박스", 
             desc: "헌책방 골목의 정취를 느낄 수 있는 빈티지한 아지트 같은 북카페입니다.", 
             location: "경기 파주시 문발로 240-21",
-            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20160614_142%2F1465888888888_142_0.jpg"
+            img: "https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20160614_142%2F1465888888888_142_0.jpg",
+            mapLink: "https://map.naver.com/v5/entry/place/1579699511",
+            blogLinks: [
+                { title: "헌책방 골목의 정취를 담은 빈티지한 북카페", url: "https://blog.naver.com/PostView.naver?blogId=bluebox&logNo=223114845612" },
+                { title: "아이와 함께 보물찾기하듯 책 고르기 좋은 곳", url: "https://blog.naver.com/PostView.naver?blogId=treasure_hunt&logNo=223214845612" },
+                { title: "파주 대표 북카페 지혜의 숲 vs 블루박스 비교", url: "https://blog.naver.com/PostView.naver?blogId=paju_compare&logNo=223314845612" }
+            ]
         }
     ]
 };
@@ -157,7 +247,22 @@ function displayResult(cafe) {
     cafeImage.src = cafe.img;
     cafeName.textContent = cafe.name;
     cafeDesc.textContent = cafe.desc;
-    cafeLocation.textContent = "📍 " + cafe.location;
+    
+    // UI for Map and Blog Links
+    let linksHtml = `<p class="location">📍 <a href="${cafe.mapLink}" target="_blank" style="color: inherit; text-decoration: underline;">${cafe.location}</a></p>`;
+    
+    if (cafe.blogLinks && cafe.blogLinks.length > 0) {
+        linksHtml += `<div class="blog-links" style="margin-top: 15px; text-align: left; font-size: 0.9rem;">
+            <p style="font-weight: bold; margin-bottom: 5px;">🔗 인기 블로그 리뷰</p>
+            <ul style="list-style: none; padding: 0;">`;
+        cafe.blogLinks.forEach(blog => {
+            linksHtml += `<li style="margin-bottom: 5px;">• <a href="${blog.url}" target="_blank" style="color: #666;">${blog.title}</a></li>`;
+        });
+        linksHtml += `</ul></div>`;
+    }
+    
+    // Replace location content
+    cafeLocation.innerHTML = linksHtml;
     
     resultArea.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
 }
